d8c3eec26bedbdfb061701cbacc5fb58d9386ed0
Joseph Lee <jin3lee@gmail.com>
2016-06-07 12:46:00 -0700

    *hides/displays fragments when users click tabs

--- TicTacToe/app/src/main/java/com/josephjinsunglee/www/tictactoe/MainActivity.java
+++ TicTacToe/app/src/main/java/com/josephjinsunglee/www/tictactoe/MainActivity.java
@@ -2,18 +2,18 @@ package com.josephjinsunglee.www.tictactoe;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentTransaction;
 
 public class MainActivity extends FragmentActivity
 {
-    Button gameButton, settingButton;
-    RelativeLayout relativeLayout_game, relativeLayout_settings;
-    boolean isGameTabActive = true;
+    Button gameButton, instructionButton;
+    RelativeLayout relativeLayout_game, relativeLayout_instruction;
+    boolean isGameTabActive;
+    RelativeLayout gameFragment_container, instructionFragment_container;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -21,52 +21,56 @@ public class MainActivity extends FragmentActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(findViewById(R.id.frameLayout_content) != null)
-        {
-            if (savedInstanceState != null) {
-                return;
-            }
-
-            GameFragment gameFragment = new GameFragment();
-            gameFragment.setArguments(getIntent().getExtras());
-
-            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-            transaction.add(R.id.frameLayout_content, gameFragment);
-            transaction.commit();
-
-            initializeViews();
-        }
+        initializeViews();
     }
 
     private void initializeViews()
     {
         // leash views
         gameButton = (Button)findViewById(R.id.button_game);
-        settingButton = (Button)findViewById(R.id.button_setting);
+        instructionButton = (Button)findViewById(R.id.button_instruction);
         relativeLayout_game = (RelativeLayout)findViewById(R.id.relativeLayout_game);
-        relativeLayout_settings = (RelativeLayout)findViewById(R.id.relativeLayout_settings);
+        relativeLayout_instruction = (RelativeLayout)findViewById(R.id.relativeLayout_instruction);
+        gameFragment_container = (RelativeLayout)findViewById(R.id.gameFragmentContainer);
+        instructionFragment_container = (RelativeLayout)findViewById(R.id.instructionFragmentContainer);
 
         // initialize tab color to orange
         relativeLayout_game.setBackgroundColor(Color.parseColor("#FF8800"));
+        isGameTabActive = true;
+        instructionFragment_container.setVisibility(View.GONE); //hide instructionFragment
 
-        // set button listeners
+
+        // when users click the instruction button the gameFragment appears & other fragment hides
         gameButton.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-                relativeLayout_game.setBackgroundColor(Color.parseColor("#FF8800"));
-                relativeLayout_settings.setBackgroundColor(Color.TRANSPARENT);
+                if(!isGameTabActive)
+                {
+                    relativeLayout_game.setBackgroundColor(Color.parseColor("#FF8800"));
+                    relativeLayout_instruction.setBackgroundColor(Color.TRANSPARENT);
+                    isGameTabActive = !isGameTabActive;
+                    instructionFragment_container.setVisibility(View.GONE);
+                    gameFragment_container.setVisibility(View.VISIBLE);
+                }
             }
         });
 
-        settingButton.setOnClickListener(new View.OnClickListener()
+        // when users click the instruction button the instructionFragment appears & gameFragment hides
+        instructionButton.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-                relativeLayout_game.setBackgroundColor(Color.TRANSPARENT);
-                relativeLayout_settings.setBackgroundColor(Color.parseColor("#FF8800"));
+                if(isGameTabActive)
+                {
+                    relativeLayout_game.setBackgroundColor(Color.TRANSPARENT);
+                    relativeLayout_instruction.setBackgroundColor(Color.parseColor("#FF8800"));
+                    isGameTabActive = !isGameTabActive;
+                    instructionFragment_container.setVisibility(View.VISIBLE);
+                    gameFragment_container.setVisibility(View.GONE);
+                }
             }
         });
     }
